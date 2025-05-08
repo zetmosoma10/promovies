706fc61ea12458a3f4a25ab7d1548f7a81a7561a
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-05-08 23:08:58 +0200

    add color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 function App() {
   return (
-    <>
-      <h1 className="text-2xl font-bold text-red-600">Hello World</h1>
-    </>
+    <section className="min-h-screen bg-gray-950">
+      <h1 className="text-5xl text-gray-100">Hello World</h1>
+      <p className="text-gray-300">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae aliquam
+        debitis cum beatae ipsum omnis rem veritatis perspiciatis, sunt
+        necessitatibus sed, repellendus deserunt sequi, impedit assumenda esse
+        fugiat! Voluptatibus cum suscipit, quaerat voluptates fugiat tempore,
+        corporis laudantium delectus consequuntur, neque eaque sit? Laborum qui
+        expedita quia nemo! Porro, nesciunt nihil.
+      </p>
+      <button className="bg-blue-600 text-lightGray">Search</button>
+      <small className="text-red-600">warning</small>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: "#1E1E1E",
+        surfaceColor: "#2C2C2C",
+        lightGray: "#E0E0E0",
+        catedGray: "#A0A0A0",
+        AzureBlue: "#3B82F6",
+        imperialRed: "#EF4444",
+        mintGreen: "#10B981",
+      },
+    },
   },
   plugins: [],
 };
