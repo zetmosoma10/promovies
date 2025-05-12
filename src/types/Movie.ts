2251f676ecccae341b77f53181fea82f493d400b
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-05-12 19:49:41 +0200

    build details page for TVShows

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -2,17 +2,18 @@ import type { Genre } from "./Genre";
 
 export type Movie = {
   id: number;
-  name?: string;
-  title?: string;
+  name: string;
+  title: string;
   revenue: number;
   runtime: number;
+  overview: string;
+  media_type: string;
+  poster_path: string;
   vote_average: number;
   release_date: string;
-  poster_path: string;
   backdrop_path: string;
-  media_type?: string;
-  overview: string;
   first_air_date: string;
+  created_by: { name: string }[];
   genres: Genre[];
   production_countries: { name: string }[];
 };
