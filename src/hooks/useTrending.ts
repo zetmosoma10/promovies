9e7b8949ea2858d310252c0dd80867c11788843a
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-05-12 11:15:21 +0200

    add pagination in trending page

--- src/hooks/useTrending.ts
+++ src/hooks/useTrending.ts
@@ -2,13 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import type { Movie } from "../types/Movie";
 import APIClient from "../services/apiClient";
 import ms from "ms";
+import type { Category } from "../types/Category";
+import useMovieStore from "../store";
 
 const apiClient = new APIClient<Movie>("/trending/all/week");
 
-const useTrending = () => {
+const useTrending = (category: Category) => {
+  const query = useMovieStore((s) => s.movieQuery[category]);
   return useQuery({
-    queryKey: ["trending"],
-    queryFn: () => apiClient.getAll(),
+    queryKey: ["Trending-All", query],
+    queryFn: () =>
+      apiClient.getAll({
+        params: {
+          page: query.page,
+        },
+      }),
     staleTime: ms("3h"),
   });
 };
--- src/pages/TrendingPage.tsx
+++ src/pages/TrendingPage.tsx
@@ -3,18 +3,17 @@ import MovieCard from "../components/MovieCard";
 import MovieCardSkeleton from "../components/MovieCardSkeleton";
 import useTrending from "../hooks/useTrending";
 import MovieHeader from "../components/MovieHeader";
+import Pagination from "../components/Pagination";
 
 const TrendingPage = () => {
-  const { data, isLoading, isError, error } = useTrending();
+  const { data, isLoading, isError, error } = useTrending("trending");
 
   if (isError) throw error;
 
   return (
     <section className="max-container">
       <div className="my-10">
-        <h1 className="figtree text-gray-50 font-semibold text-5xl">
-          Trending
-        </h1>
+        <MovieHeader category="trending">Trending</MovieHeader>
       </div>
       <div className="mt-5 grid-container">
         {isLoading
@@ -24,6 +23,9 @@ const TrendingPage = () => {
                 <MovieCard key={movie.id} movie={movie} />
               </Link>
             ))}
+      </div>
+      <div className="flex items-center justify-center mt-10">
+        <Pagination category="trending" />
       </div>
     </section>
   );
