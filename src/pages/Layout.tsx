62db5e47187747be10e6bc3aa4ce4d05567c8e80
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-05-14 20:55:35 +0200

    addd scroll to top functionality

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Layout = () => {
   return (
     <section className="min-h-screen bg-bgColor pb-10">
       <NavBar />
       <Outlet />
+      <ScrollToTop />
     </section>
   );
 };
