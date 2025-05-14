bdfbd445e22a9cdd080366ab0374819763fd6080
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-05-14 20:41:07 +0200

    implement infinite scroll in search page

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,18 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import APIClient from "../services/apiClient";
 import type { Movie } from "../types/Movie";
 
 const useSearch = (query: string | null) => {
   const apiClient = new APIClient<Movie>("/search/multi");
 
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: ["search", query],
-    queryFn: () =>
+    queryFn: ({ pageParam }) =>
       apiClient.getAll({
         params: {
           query,
+          page: pageParam,
         },
       }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      return lastPage.page < lastPage.total_pages
+        ? lastPage.page + 1
+        : undefined;
+    },
+    staleTime: ms("1h"),
+    refetchOnWindowFocus: false,
   });
 };
 
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,38 +3,65 @@ import MovieCard from "../components/MovieCard";
 import useSearch from "../hooks/useSearch";
 import MovieCardSkeleton from "../loadingSkeletons/MovieCardSkeleton";
 import generateSlug from "../services/generateSlug";
+import { useInView } from "react-intersection-observer";
+import React, { useEffect } from "react";
 
 const SearchPage = () => {
   const [searchParam] = useSearchParams();
   const query = searchParam.get("query");
-  const { data, isLoading, isError, error } = useSearch(query);
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useSearch(query);
+
+  const { inView, ref } = useInView({ threshold: 0.5 });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   if (isError) throw error;
-
-  const movies = data?.results.filter((m) => m.media_type !== "person");
 
   return (
     <section className="font-semibold max-container text-gray-50">
       <h1 className="mt-10 mb-8 text-3xl font-semibold">
-        {data?.total_results === 0
+        {data?.pages.length === 0
           ? `No results for "${query}"`
           : `Search results for "${query}"`}
       </h1>
       <div className="relative grid-container">
         {isLoading
           ? [...Array(10)].map((_, index) => <MovieCardSkeleton key={index} />)
-          : movies?.map((movie) => (
-              <Link
-                to={
-                  movie.media_type === "movie"
-                    ? `/movies/${generateSlug(movie)}`
-                    : `/tv-shows/${generateSlug(movie)}`
-                }
-                key={movie.id}
-              >
-                <MovieCard key={movie.id} movie={movie} />
-              </Link>
+          : data?.pages.map((page, index) => (
+              <React.Fragment key={index}>
+                {page.results
+                  .filter((m) => m.media_type !== "person")
+                  .map((movie) => (
+                    <Link
+                      to={
+                        movie.media_type === "movie"
+                          ? `/movies/${generateSlug(movie)}`
+                          : `/tv-shows/${generateSlug(movie)}`
+                      }
+                      key={movie.id}
+                    >
+                      <MovieCard key={movie.id} movie={movie} />
+                    </Link>
+                  ))}
+              </React.Fragment>
             ))}
+      </div>
+      <div ref={ref}>
+        {isFetchingNextPage && (
+          <p className="text-gray-50 mt-5 text-center">Loading...</p>
+        )}
       </div>
     </section>
   );
